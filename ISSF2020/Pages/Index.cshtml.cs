90cae07a0857e2ff7e21b5ce323e36d1a8b31cf3
kashyl <staviladeian@gmail.com>
2020-04-30 03:14:15 +0100

    Add project files.

---
+++ ISSF2020/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ISSF2020.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [TempData]
+        public string Message { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+            /*
+            ViewData["MyNumber"] = 42;
+            ViewData["MyString"] = "Hello World";
+            */
+            ViewData["User"] = HttpContext.Session.GetString("User");
+
+        }
+    }
+}
